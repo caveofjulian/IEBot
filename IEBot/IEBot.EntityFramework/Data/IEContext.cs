dd42c092d82e444dd7186b695eb7b2591b3c55bb
Julian <495328jf@eur.nl>
2019-09-19 16:33:38 +0200

    updated local password

--- IEBot/IEBot.EntityFramework/Data/IEContext.cs
+++ IEBot/IEBot.EntityFramework/Data/IEContext.cs
@@ -13,7 +13,7 @@ namespace IEBot.EntityFramework.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseMySQL(@"Server=localhost;Database=IEBot;Uid=julian;Pwd=hoihoi123");
+            builder.UseMySQL(@"Server=localhost;Database=IEBot;Uid=julian;Pwd=Hoihoi_123");
         }
     }
 }
